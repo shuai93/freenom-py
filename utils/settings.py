68121a8b7d3a1a21f4758c1988409c5835306b96
杨帅 <shuai.yang001@yeah.net>
2021-05-13 18:51:52 +0800

    修改配置

--- utils/settings.py
+++ utils/settings.py
@@ -1,12 +1,12 @@
 import os
 
 # qq mail
-MAIL_TO = os.getenv("MAIL_TO", "")
-MAIL_HOST = os.getenv("SMTP_HOST", "")
-MAIL_PORT = int(os.getenv("SMTP_PORT", 0))
-MAIL_USER = os.getenv("MAIL_USER", "")
 MAIL_ADDRESS = os.getenv("MAIL_ADDRESS", "")
+MAIL_HOST = os.getenv("SMTP_HOST", "")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
+MAIL_PORT = int(os.getenv("SMTP_PORT", 0))
+MAIL_TO = os.getenv("MAIL_TO", "")
+MAIL_USER = os.getenv("MAIL_USER", "")
 
 
 # free nom
