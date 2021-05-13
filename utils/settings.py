e1c79b36317b9b15153f9b50a4317e94c81b4e5e
杨帅 <shuai.yang001@yeah.net>
2021-05-13 19:02:25 +0800

    新增输出

--- utils/settings.py
+++ utils/settings.py
@@ -2,9 +2,9 @@ import os
 
 # qq mail
 MAIL_ADDRESS = os.getenv("MAIL_ADDRESS", "")
-MAIL_HOST = os.getenv("SMTP_HOST", "")
+MAIL_HOST = os.getenv("SMTP_HOST", "smtp.qq.com")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
-MAIL_PORT = int(os.getenv("SMTP_PORT", 0))
+MAIL_PORT = int(os.getenv("SMTP_PORT", 465))
 MAIL_TO = os.getenv("MAIL_TO", "")
 MAIL_USER = os.getenv("MAIL_USER", "")
 
